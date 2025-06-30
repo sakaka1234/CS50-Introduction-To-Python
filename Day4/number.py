2ad8004c340a120096b801615190771c4ebc1285
sathegoodkid <sa@gmail.com>
2025-07-01 00:10:10 +0700

    Done day 4

---
+++ Day4/number.py
@@ -0,0 +1,13 @@
+def main():
+    x = get_int("What's x ? ")
+    print(x)
+
+def get_int(prompt):
+    while True:
+        try:
+            return  int(input(prompt))
+            
+        except ValueError:
+            continue
+        
+main()
